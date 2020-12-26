06500204b3dd92051599a563962f4b3a6f6deaaf
SKProCH <skproch@yandex.ru>
2020-12-26 14:01:02 +0300

    Make GUI working

---
+++ WorldsMergerGui/Models/ReactiveLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using Microsoft.Extensions.Logging;
+
+namespace WorldsMergerGui.Models {
+    public class ReactiveLogger : ILogger {
+        public ISubject<string> OnLog { get; } = new Subject<string>();
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
+            OnLog.OnNext($"{DateTime.Now} | [{logLevel}] | {formatter(state, exception)}");
+        }
+
+        public bool IsEnabled(LogLevel logLevel) {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) {
+            return Disposable.Empty;
+        }
+    }
+}
--- WorldsMergerGui/ViewModels/MainWindowViewModel.cs
+++ WorldsMergerGui/ViewModels/MainWindowViewModel.cs
@@ -3,26 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Microsoft.Extensions.Logging;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using WorldsMergerGui.Models;
 
 namespace WorldsMergerGui.ViewModels {
     public class MainWindowViewModel : ViewModelBase {
+        public ISubject<bool> CanRunProcess = new Subject<bool>();
+        private ReactiveLogger _reactiveLogger;
+
         public MainWindowViewModel() {
-            BrowseOldLevelData  = ReactiveCommand.Create<Window, Task>(async window => {
-                OldLevelData = await BrowsePath(window, OldLevelData);
-            }); 
-            BrowseNewLevelData = ReactiveCommand.Create<Window, Task>(async window => {
-                NewLevelData = await BrowsePath(window, NewLevelData);
-            }); 
-            BrowsePlayerData = ReactiveCommand.Create<Window, Task>(async window => {
-                PlayerData = await BrowsePath(window, PlayerData);
-            }); 
-            BrowseOutputPath = ReactiveCommand.Create<Window, Task>(async window => {
-                OutputPath = await BrowsePath(window, OutputPath);
-            }); 
+            BrowseOldLevelData = ReactiveCommand.Create<Window, Task>(async window => { OldLevelData = await BrowsePath(window, OldLevelData); });
+            BrowseNewLevelData = ReactiveCommand.Create<Window, Task>(async window => { NewLevelData = await BrowsePath(window, NewLevelData); });
+            BrowsePlayerData = ReactiveCommand.Create<Window, Task>(async window => { PlayerData = await BrowsePath(window, PlayerData); });
+            BrowseOutputPath = ReactiveCommand.Create<Window, Task>(async window => { OutputPath = await BrowsePath(window, OutputPath); });
+            RunCommand = ReactiveCommand.Create(Run, CanRunProcess.ObserveOn(RxApp.MainThreadScheduler));
+            CanRunProcess.OnNext(true);
+            _reactiveLogger = new ReactiveLogger();
+            _reactiveLogger.OnLog.Subscribe(s => LogText += s + Environment.NewLine);
+        }
+
+        private void Run() {
+            CanRunProcess.OnNext(false);
+            Task.Run(() => {
+                LogText = "";
+                _reactiveLogger.Log(LogLevel.Information, "Starting...");
+                try {
+                    WorldsMergerCli.WorldsMerger.Process(PlayerData, OldLevelData, NewLevelData, OutputPath, _reactiveLogger);
+                }
+                catch (Exception e) {
+                    _reactiveLogger.Log(LogLevel.Critical, e.ToString());
+                }
+                finally {
+                    CanRunProcess.OnNext(true);
+                }
+            });
         }
 
         private async Task<string> BrowsePath(Window window, string oldPath) {
@@ -35,17 +56,21 @@ namespace WorldsMergerGui.ViewModels {
             return files.Length == 0 ? oldPath : files.First();
         }
 
-        public string OldLevelData { get; set; }
+        [Reactive] public string OldLevelData { get; set; }
 
-        public string NewLevelData { get; set; }
+        [Reactive] public string NewLevelData { get; set; }
 
-        public string PlayerData { get; set; }
+        [Reactive] public string PlayerData { get; set; }
 
-        public string OutputPath { get; set; }
+        [Reactive] public string OutputPath { get; set; }
 
         public ReactiveCommand<Window, Task> BrowseOldLevelData { get; set; }
         public ReactiveCommand<Window, Task> BrowseNewLevelData { get; set; }
         public ReactiveCommand<Window, Task> BrowsePlayerData { get; set; }
         public ReactiveCommand<Window, Task> BrowseOutputPath { get; set; }
+
+        [Reactive] public string LogText { get; set; }
+
+        public ReactiveCommand<Unit, Unit> RunCommand { get; set; }
     }
 }
